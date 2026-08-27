0a6952dcd4c7c59002d185a04398eab7bf48af25
Nageswara Rao Samayam <nsamayam@mypassglobal.com>
2026-08-28 03:42:02 +0530

    WIP: update docker compose file

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,6 @@ use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Http\Middleware\PreventRequestForgery;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello-world', fn () => 'Hello world');
-Route::get('/hello-world12', fn () => 'Hello world...123');
-
-Route::get('/test-live', fn () => 'It works instantly now!');
-
 Route::get('/', function () {
     return view('welcome');
 });
