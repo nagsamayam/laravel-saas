9f2bfd446d307ecfe8319af1d757f06330beacaf
Nageswara Rao Samayam <nsamayam@mypassglobal.com>
2026-08-29 16:14:49 +0530

    Testing app versioning

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::post('/api/v1/register', [UserController::class, 'register'])->withoutMiddleware([PreventRequestForgery::class]);
